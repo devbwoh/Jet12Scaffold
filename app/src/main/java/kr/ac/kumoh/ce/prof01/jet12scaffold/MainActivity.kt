7c02226f0d72ca8ea3248e3cfc1a965236f7bc24
devbwoh <bwoh@outlook.kr>
2024-01-23 03:54:36 +0900

    🎉 시작

---
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet12scaffold/MainActivity.kt
@@ -0,0 +1,42 @@
+package kr.ac.kumoh.ce.prof01.jet12scaffold
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.ce.prof01.jet12scaffold.ui.theme.Jet12ScaffoldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Jet12ScaffoldTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    Jet12ScaffoldTheme {
+        MainScreen()
+    }
+}
