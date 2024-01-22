94ee339f7594f87fe8fad3119a5983821b329f0a
devbwoh <bwoh@outlook.kr>
2024-01-23 04:26:34 +0900

    ✨ Snackbar

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet12scaffold/Song.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet12scaffold/Song.kt
@@ -17,6 +17,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -26,19 +28,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 import kr.ac.kumoh.ce.prof01.jet12scaffold.ui.theme.Jet12ScaffoldTheme
 
 @Composable
 fun SongScreen() {
+    val snackbar = remember {
+        SnackbarHostState()
+    }
     Scaffold(
         topBar = { SongTopBar() },
-        bottomBar = { SongBottomBar() },
-        snackbarHost = {},
+        bottomBar = { SongBottomBar(snackbar) },
+        snackbarHost = { SnackbarHost(snackbar) },
         floatingActionButton = {},
     ) {
         Column(
@@ -93,10 +101,12 @@ fun SongTopBar() {
 }
 
 @Composable
-fun SongBottomBar() {
+fun SongBottomBar(snackbar: SnackbarHostState) {
     var searchText by rememberSaveable {
         mutableStateOf("")
     }
+    val scope = rememberCoroutineScope()
+
     TextField(
         value = searchText,
         onValueChange = {
@@ -104,7 +114,11 @@ fun SongBottomBar() {
         },
         leadingIcon = {
             IconButton(
-                onClick = {}
+                onClick = {
+                    scope.launch {
+                        snackbar.showSnackbar("노래 $searchText 검색")
+                    }
+                }
             ) {
                 Icon(
                     Icons.Default.Search,
